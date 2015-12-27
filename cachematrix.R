20083c0b5ce7a20887b854efbae79dc551a528b3
e.a. nakashima <e.a.nakashima@gmail.com>
2015-12-27 13:19:54 -0800

    Add assignment 2 implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion can be a costly calculation. This pair of functions caches
+# the inverse of a matrix rather than computing it repeatedly.
 
-## Write a short comment describing this function
-
+# This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+    # Make an empty variable to hold the cached matrix
+    cache <- NULL
 
+    # The set function sets the value of the value of the matrix to be cached
+    set <- function(y) {
+        x <<- y
+        cache <<- NULL
+    }
+
+    # The get function gets the value of the value of the matrix to be cached
+    get <- function() x
+
+    # The setsolve function sets the inverted matrix to the cache
+    setsolve <- function(solve) cache <<- solve
+
+    # The getsolve function gets the inverted matrix from the cache (may be null)
+    getsolve <- function() cache
+
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+# This function computes the inverse of the "matrix" returned by
+# `makeCacheMatrix` above. If the inverse has already been calculated (and the
+# matrix has not changed), then cacheSolve retrieves the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Call the cache fetch function to look for a cached result
+    cache <- x$getsolve()
+
+    # Check if the result is cached, if it is, print a message and return early
+    # with the cached result.
+    if(!is.null(cache)) {
+        message("getting cached data")
+        return(cache)
+    }
+
+    # If the result is not cached, we'll have to take the time to solve it.
+    data <- x$get()
+    cache <- solve(data, ...)
+
+    # Then we save the newly calculated result so it will be cached for next time.
+    x$setsolve(cache)
+
+    # Finally we return the new result.
+    cache
 }
